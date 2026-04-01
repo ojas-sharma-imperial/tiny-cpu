54bed0ad27d15cb7673bd1736efaaca4fdf4eab1
railwaysaregreat <ojas.sharma25@imperial.ac.uk>
2026-04-01 09:14:16 +0100

    possibly working?

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_ojas_sharma_imperial_ttcpu user_project (
+  tt_um_ojas_sharma_imperial_ttcpu dut (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
